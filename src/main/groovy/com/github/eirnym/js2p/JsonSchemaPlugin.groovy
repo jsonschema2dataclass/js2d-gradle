2012503c188559375bb2b2383df4050048e9328a
Eir Nym <485399+eirnym@users.noreply.github.com>
2021-03-22 22:51:08 +0100

    Split by methods for readability

--- src/main/groovy/com/github/eirnym/js2p/JsonSchemaPlugin.groovy
+++ src/main/groovy/com/github/eirnym/js2p/JsonSchemaPlugin.groovy
@@ -35,9 +35,21 @@ class JsonSchemaPlugin implements Plugin<Project> {
 
         project.extensions.create('jsonSchema2Pojo', JsonSchemaExtension)
 
+        if (project.plugins.hasPlugin('java')) {
+            applyJava(project)
+        } else if (project.plugins.hasPlugin('com.android.application') || project.plugins.hasPlugin('com.android.library')) {
+            project.afterEvaluate { applyAndroid(project) }
+        } else {
+            for (Plugin<?> plugin : project.plugins) {
+                project.logger.error(plugin.class.name)
+            }
+            throw new GradleException("${TASK_NAME}: Java or Android plugin required")
+        }
+    }
+
+    private static void applyJava(Project project) {
         project.plugins.withId('java') {
             JsonSchemaExtension config = project.extensions.getByType(JsonSchemaExtension)
-            ConfigurableFileCollection sourceFiles
             JavaPluginConvention javaConvention = project.getConvention().plugins['java'] as JavaPluginConvention
 
             if (config.source.isEmpty()) {
@@ -57,58 +69,45 @@ class JsonSchemaPlugin implements Plugin<Project> {
                 it.dependsOn(js2pTask)
             }
         }
-        project.afterEvaluate {
-            if (project.plugins.hasPlugin('java')) {
-                // do nothing
-            } else if (project.plugins.hasPlugin('com.android.application') || project.plugins.hasPlugin('com.android.library')) {
-                def variants
-                if (project.android.hasProperty('applicationVariants')) {
-                    variants = project.android.applicationVariants
-                } else if (project.android.hasProperty('libraryVariants')) {
-                    variants = project.android.libraryVariants
-                } else {
-                    throw new IllegalStateException('Android project must have applicationVariants or libraryVariants!')
-                }
+    }
 
-                JsonSchemaExtension config = project.extensions.getByType(JsonSchemaExtension)
-
-                if (System.getProperty('java.specification.version').toFloat() >= 9) {
-                    config.includeGeneratedAnnotation = false  // Temporary fixes #71 and upstream issue #1212 (used Generated annotation is not compatible with AGP 7+)
-                }
-
-                ConfigurableFileCollection sourceFiles
-
-                if (config.source.isEmpty()) {
-                    def sets
-                    if (project.android.hasProperty('sourceSets')) {
-                        sets = project.android.sourceSets;
-                    } else {
-                        sets = project.android.sourceSetsContainer;
-                    }
-
-                    sets.all { sourceSet ->
-                        if(sourceSet.name.startsWith("main")){
-                            def path = Paths.get(sourceSet.resources.source[0], 'json')
-                            config.source.from(path)
-                        }
-                    }
-                }
-
-                variants.all { variant ->
-                    GenerateFromJsonSchemaTask task = createJS2DTask(
-                            project,
-                            "${TASK_NAME}For${variant.name.capitalize()}",
-                            config.source,
-                            "$BASE_FOLDER/${variant.flavorName}/${variant.buildType.name}/"
-                    )
-                    variant.registerJavaGeneratingTask(task, task.targetDirectory.get().asFile)
-                }
+    private static void applyAndroid(Project project) {
+        def variants
+        if (project.android.hasProperty('applicationVariants')) {
+            variants = project.android.applicationVariants
+        } else if (project.android.hasProperty('libraryVariants')) {
+            variants = project.android.libraryVariants
+        } else {
+            throw new IllegalStateException('Android project must have applicationVariants or libraryVariants!')
+        }
+        JsonSchemaExtension config = project.extensions.getByType(JsonSchemaExtension)
+        if (System.getProperty('java.specification.version').toFloat() >= 9) {
+            config.includeGeneratedAnnotation = false
+            // Temporary fixes #71 and upstream issue #1212 (used Generated annotation is not compatible with AGP 7+)
+        }
+        if (config.source.isEmpty()) {
+            def sets
+            if (project.android.hasProperty('sourceSets')) {
+                sets = project.android.sourceSets;
             } else {
-                for (Plugin<?> plugin : project.plugins) {
-                    project.logger.error(plugin.class.name)
-                }
-                throw new GradleException('generateJsonSchema: Java or Android plugin required')
+                sets = project.android.sourceSetsContainer;
             }
+
+            sets.all { sourceSet ->
+                if (sourceSet.name.startsWith("main")) {
+                    def path = Paths.get(sourceSet.resources.source[0], 'json')
+                    config.source.from(path)
+                }
+            }
+        }
+        variants.all { variant ->
+            GenerateFromJsonSchemaTask task = createJS2DTask(
+                    project,
+                    "${TASK_NAME}For${variant.name.capitalize()}",
+                    config.source,
+                    "$BASE_FOLDER/${variant.flavorName}/${variant.buildType.name}/"
+            )
+            variant.registerJavaGeneratingTask(task, task.targetDirectory.get().asFile)
         }
     }
 
