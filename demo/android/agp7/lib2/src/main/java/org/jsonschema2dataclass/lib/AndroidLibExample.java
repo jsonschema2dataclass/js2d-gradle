e5255b5cbd4fa526d2b9f67f5b514c001b8d6e8d
Eir Nym <485399+eirnym@users.noreply.github.com>
2021-12-16 00:03:38 +0100

    Gradle 7.3.2 for plugin and most of demos

--- demo/android/agp7/lib2/src/main/java/org/jsonschema2dataclass/lib/AndroidLibExample.java
+++ demo/android/agp7/lib2/src/main/java/org/jsonschema2dataclass/lib/AndroidLibExample.java
@@ -1,14 +1,14 @@
 package org.jsonschema2dataclass.lib;
 
-import com.oosocial.clarityn.rest.clarityn.model.Entry_schema;
+import com.oosocial.clarityn.rest.clarityn.model.EntrySchema;
 import com.oosocial.clarityn.rest.clarityn.model.Storage;
 
 public final class AndroidLibExample {
 
   public static void example() {
-    Entry_schema schema = new Entry_schema();
+    EntrySchema schema = new EntrySchema();
     schema.setReadonly(false);
-    schema.setFstype(Entry_schema.Fstype.EXT_4);
+    schema.setFstype(EntrySchema.Fstype.EXT_4);
     schema.setStorage(new Storage());
   }
 }
