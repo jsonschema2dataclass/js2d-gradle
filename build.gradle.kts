2e4168d3114fa91d8a0900ba4e3168a8ee8fc9a1
Eir Nym <485399+eirnym@users.noreply.github.com>
2022-12-24 16:06:11 +0100

    Fix Gradle 8 compatibility issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,16 +26,10 @@ repositories {
     mavenCentral()
 }
 
-pluginBundle {
-    website = "https://github.com/jsonschema2dataclass/js2d-gradle"
-    vcsUrl = "https://github.com/jsonschema2dataclass/js2d-gradle.git"
-    tags = listOf(
-        "json-schema", "jsonschema", "generator", "pojo", "jsonschema2pojo",
-        "dataclass", "data", "json", "generation", "jsonschema2dataclass", "java"
-    )
-}
-
 gradlePlugin {
+    website.set("https://github.com/jsonschema2dataclass/js2d-gradle")
+    vcsUrl.set("https://github.com/jsonschema2dataclass/js2d-gradle.git")
+
     plugins {
         create("jsonschema2dataclassPlugin") {
             id = "org.jsonschema2dataclass"
@@ -43,7 +37,15 @@ gradlePlugin {
             implementationClass = "org.jsonschema2dataclass.js2p.Js2pPlugin"
             displayName = "Extended Gradle JsonSchema2Pojo Plugin"
             description =
-                "A plugins that generates Java sources from JsonSchema using jsonschema2pojo. Please, see the GitHub page for details"
+                "A plugins that generates Java sources from JsonSchema using jsonschema2pojo. " +
+                "Please, see the GitHub page for details"
+
+            tags.set(
+                listOf(
+                    "json-schema", "jsonschema", "generator", "pojo", "jsonschema2pojo",
+                    "dataclass", "data", "json", "generation", "jsonschema2dataclass", "java"
+                )
+            )
         }
     }
 }
