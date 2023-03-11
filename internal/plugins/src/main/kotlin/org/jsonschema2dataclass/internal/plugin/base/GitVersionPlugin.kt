474c95dc7c876544c655d9659e9fbaad5ee25fec
Eir Nym <485399+eirnym@users.noreply.github.com>
2023-03-11 11:12:19 +0100

    Internal plugin fixes
    * Put no-git-version extra to utils
    * Don't configure spotless if not enabled

--- internal/plugins/src/main/kotlin/org/jsonschema2dataclass/internal/plugin/base/GitVersionPlugin.kt
+++ internal/plugins/src/main/kotlin/org/jsonschema2dataclass/internal/plugin/base/GitVersionPlugin.kt
@@ -1,15 +1,14 @@
 package org.jsonschema2dataclass.internal.plugin.base
 
+import EXTRA_NO_GIT_VERSION
 import isExtraEnabled
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-private const val NO_GIT_VERSION_EXTRA = "org.jsonschema2dataclass.local"
-
 @Suppress("unused")
 class GitVersionPlugin : Plugin<Project> {
     override fun apply(project: Project) {
-        if (project.isExtraEnabled(NO_GIT_VERSION_EXTRA)) {
+        if (project.isExtraEnabled(EXTRA_NO_GIT_VERSION)) {
             project.version = "1.0-SNAPSHOT"
         } else {
             project.version = gitVersion(project)
