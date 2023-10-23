687b7a9f26b14790164e13c801538812004009ab
Eir Nym <485399+eirnym@users.noreply.github.com>
2023-10-23 11:02:02 +0200

    Increase compileSdkVersion to 34 in Android demo

--- demo/android-agp7/lib2/build.gradle.kts
+++ demo/android-agp7/lib2/build.gradle.kts
@@ -6,11 +6,11 @@ plugins {
 
 android {
     namespace = "org.jsonschema2dataclass.lib2"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
