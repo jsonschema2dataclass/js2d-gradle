6057e1d0f88254f540c3e993ebc0d15c0fed0719
Eir Nym <485399+eirnym@users.noreply.github.com>
2024-02-12 21:04:40 +0100

    Disable Spotless and git version by default. Rename version override variable

--- internal/plugins/src/main/kotlin/org/jsonschema2dataclass/internal/plugin/base/GitVersion.kt
+++ internal/plugins/src/main/kotlin/org/jsonschema2dataclass/internal/plugin/base/GitVersion.kt
@@ -1,5 +1,9 @@
 package org.jsonschema2dataclass.internal.plugin.base
 
+import EXTRA_GIT_VERSION_ENABLE
+import EXTRA_GIT_VERSION_OVERRIDE
+import extraValue
+import isExtraEnabled
 import org.gradle.api.Project
 import java.io.IOException
 import java.nio.charset.StandardCharsets
@@ -10,22 +14,27 @@ import java.util.concurrent.TimeUnit
 
 private val regex = Regex("""^v?([0-9.]*(?:-rc\d+)?)-(\d+)-g([0-9a-f]+)(-dirty)?$""")
 private const val DEFAULT_VERSION = "-.-.--0-g00000000-dirty"
-private const val PROPERTY_SKIP_GIT_QUERY = "org.jsonschema2dataclass.internal.no-git-version"
 
 fun gitVersion(project: Project): String {
-    val commandVersion = when (project.properties[PROPERTY_SKIP_GIT_QUERY] == "true") {
-        true -> null
-        false -> commandVersion(project)
+    val versionOverride = project.extraValue(EXTRA_GIT_VERSION_OVERRIDE)
+    if (versionOverride != null) {
+        return versionOverride
+    }
+
+    val commandVersion = when (project.isExtraEnabled(EXTRA_GIT_VERSION_ENABLE)) {
+        true -> commandVersion(project)
+        false -> null
     }
     return processVersionString(commandVersion ?: DEFAULT_VERSION)
 }
 
 private fun processVersionString(value: String): String {
-    if (!regex.matches(value)) {
+    val match = regex.find(value)
+    if (match == null) {
         val now = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd.HH-mm-ss", Locale.US))
         return "0.0.0-$now"
     }
-    val match = regex.find(value)!!
+
     val (version, commitsAfterTag, revision, dirty) = match.destructured
 
     return when (commitsAfterTag) {
