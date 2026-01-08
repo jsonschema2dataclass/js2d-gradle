c42b93d29193c7d7ae1e6f5378a5baae4f4e1e54
Eir Nym <485399+eirnym@users.noreply.github.com>
2026-01-08 17:28:46 +0100

    Remove obsolete files

--- plugin-gradle/compat/kotlin/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-plugins {
-    `kotlin-dsl-base`
-    id("org.jsonschema2dataclass.internal.library")
-}
-
-base.archivesName.set("jsonschema2dataclass-kotlin-compat")
-description = "Plugin Kotlin Compat: Kotlin compatibility functions"
